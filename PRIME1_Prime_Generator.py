6ab6965689e803239ccd8dfadf656234301b0cb3
hichamdz38 <dachirhicham@gmail.com>
2021-03-27 23:53:29 +0100

    optimization Prime1 test only previous found primes

--- PRIME1_Prime_Generator.py
+++ PRIME1_Prime_Generator.py
@@ -8,13 +8,14 @@ inputs:
 
 for t in range(int(input())):
     m , n = list(map(int,input().split()))
+    primes = set()
     for number in range(m, n+1):
         stat = True
-        for i in range(2,int(number**0.5)+1):
-            # print('=======>', number, i, number%i)
+        for i in primes:
             if not(number%i):
                 stat = False
                 break
         if stat and number>1:
+            primes.add(number)
             print(number)
     print()
